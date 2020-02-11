06112075c201249c9b289865639646c9fee69c29
Thomas Thio <thomasthio.kitsun@gmail.com>
2020-02-12 08:07:04 +0900

    Added a rolling history of revisions
    The rolling history allows us to see if the sampling is 'random enough'. Additionally, we can use that to recall the Japanese-English relationships.

--- LangRevision.py
+++ LangRevision.py
@@ -22,12 +22,15 @@ df.dropna(subset=['Romaji'],inplace=True)
 df.dropna(subset=['English Meaning'],inplace=True)
 
 
+
 def sample(df):
+	rolling_arr = []
+
 	#randomizer	
 	while True:
-		print("===============================")
-		input("Press for revision")
-		sampled = df.sample(n=1,replace=False)
+		input("Press for next word ===============================")
+		sampled = df.sample(n=1,replace=True) #sample without replacement does not seem to be working properly
+
 		jap_trigger = sampled[['Romaji','Hira/Kata']].iloc[0]
 		eng_trigger = sampled[['English Meaning','Category','Topic']].iloc[0]
 
@@ -37,12 +40,24 @@ def sample(df):
 		print(line_break)
 
 
-		input("Press for Answer")
-		print("===============================")
-		print(line_break)
+		input("Press for Ans >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+		if len(rolling_arr) > 3:
+			print(line_break)
+			print("Last Mem Words were")
+			for s in rolling_arr:
+				print(s)
+			print(line_break)
+
+			rolling_arr = rolling_arr[2:]
+		input(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")			
 		print(jap_trigger['Romaji'])
 		print(jap_trigger['Hira/Kata'])
 		print(line_break)
+
+
+		ele = str(eng_trigger['English Meaning']) + " | " + str(jap_trigger['Romaji']) + " | " + str(jap_trigger['Hira/Kata'])
+		rolling_arr.append(ele)
+		# print(rolling_arr)
 
 def run_program(df):
 	while True:
@@ -52,12 +67,13 @@ def run_program(df):
 		print("1: Opposites, Scales")
 		print("#2: Vocab")
 		print("#3: Grammar & Sentence Forming")
+		print("#4: Conversation Usage")
 		mode_input = int(input("Select Mode:"))
 		try:
 			if mode_input == 1:
 				print("Selected #1: Adjective Opposites, Scales")
 				#filter for adjective opposites only
-				df = df[df['Category']=='Adj']
+				df = df[df['Topic']=='Opposites']
 			elif mode_input == 2:
 				print("Selected #2: Vocabulary")
 				#filter for adjective opposites only
@@ -65,6 +81,10 @@ def run_program(df):
 
 			elif mode_input == 3:
 				print("Selected #3: Grammar & Sentence Forming")
+
+			elif mode_input == 4:
+				print("Selected #4: Conversation Use")
+				df = df[df['Category'] == 'Convo']				
 			    #todo: TBC
 			sample(df)
 		except ValueError:
